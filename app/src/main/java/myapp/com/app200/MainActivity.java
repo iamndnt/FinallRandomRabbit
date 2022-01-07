92758b9ffe30937534bc960ae1d92c84183015b8
iamndnt <duytan8b@gmail.com>
2022-01-07 12:39:27 +0700

    Initial commit

---
+++ app/src/main/java/myapp/com/app200/MainActivity.java
@@ -0,0 +1,158 @@
+package myapp.com.app200;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.Random;
+
+import myapp.com.app200.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton btn0,btn1,btn2,btn3,btn4,end;
+    Button mainbut;
+
+
+    int pos=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        thamchieu();
+        final float x=end.getX();
+        final float y=end.getY();
+
+        mainbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainbut.setClickable(false);
+                Random random = new Random();
+                int num= random.ints(1, 5).findFirst().getAsInt();
+
+                if(num==1)
+                {
+                    xuly1();
+                }
+                else if(num==2)
+                {
+                    xuly2();
+                }
+                else if(num==3)
+                {
+                    xuly3();
+                }
+                else
+                    xuly4();
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xulyvebandau();
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xulyvebandau();
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xulyvebandau();
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                xulyvebandau();
+            }
+        });
+
+    }
+
+    void xulyvebandau()
+    {
+        btn0.animate().x(end.getX()).y(end.getY()).setDuration(200).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                btn0.setX(end.getX());
+                btn0.setY(end.getY());
+            }
+        }).start();
+
+        mainbut.setClickable(true);
+    }
+
+    void xuly1()
+    {
+        btn0.animate().x(btn1.getX()).y(btn1.getY()).setDuration(2000).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                btn0.setX(btn1.getX());
+                btn0.setY(btn1.getY());
+            }
+        }).start();
+    }
+
+    void xuly2()
+    {
+        btn0.animate().x(btn2.getX()).y(btn2.getY()).setDuration(2000).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                btn0.setX(btn2.getX());
+                btn0.setY(btn2.getY());
+            }
+        }).start();
+    }
+
+    void xuly3()
+    {
+        btn0.animate().x(btn3.getX()).y(btn3.getY()).setDuration(2000).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                btn0.setX(btn3.getX());
+                btn0.setY(btn3.getY());
+            }
+        }).start();
+    }
+
+    void xuly4()
+    {
+        btn0.animate().x(btn4.getX()).y(btn4.getY()).setDuration(2000).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                btn0.setX(btn4.getX());
+                btn0.setY(btn4.getY());
+            }
+        }).start();
+    }
+
+
+
+
+    private void thamchieu()
+    {
+        btn0=findViewById(R.id.btn0);
+        btn1=findViewById(R.id.btn1);
+        btn2=findViewById(R.id.btn2);
+        btn3=findViewById(R.id.btn3);
+        btn4=findViewById(R.id.btn4);
+        end=findViewById(R.id.end);
+
+        mainbut=findViewById(R.id.mainbut);
+
+    }
+}
